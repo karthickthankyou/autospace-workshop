c668dcd4f531a250071aa051dc20950cee45122f
Karthick Ragavendran <karthickthankyou@gmail.com>
2024-04-01 20:01:43 +0530

    Import from library

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { add } from '@autospace/sample-lib';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! ' + add(23432, 223);
   }
 }
