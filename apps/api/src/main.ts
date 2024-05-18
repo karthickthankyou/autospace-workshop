ab633ad95373e9a4311f5f92e5ce81f398fc270e
Karthick Ragavendran <karthickthankyou@gmail.com>
2024-05-19 04:21:55 +0530

    Allow available network interfaces

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+const port = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -25,6 +26,6 @@ You might also need to use the <a target="_blank" href="https://studio.apollogra
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('/', app, document)
 
-  await app.listen(3000)
+  await app.listen(port, '0.0.0.0')
 }
 bootstrap()
