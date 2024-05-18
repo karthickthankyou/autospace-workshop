9cd6ed08e54f1369cd38848a46c770e310017275
Karthick Ragavendran <karthickthankyou@gmail.com>
2024-05-19 03:52:20 +0530

    Remove sample lib from api

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common'
-import { add } from '@autospace/sample-lib'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! ' + add(23432, 223)
+    return 'Hello World! '
   }
 }
