696ba35118e745ca2cab355c586d35c7107f08b7
Karthick Ragavendran <karthickthankyou@gmail.com>
2024-05-21 08:51:54 +0530

    Fix token routes

--- apps/web-admin/src/app/api/auth/token/route.ts
+++ apps/web-admin/src/app/api/auth/token/route.ts
@@ -3,7 +3,14 @@ import { cookies } from 'next/headers'
 
 export async function GET(req: NextRequest, res: NextResponse) {
   const getCookies = cookies()
-  const nextAuthSession = getCookies.get('next-auth.session-token')?.value || ''
+  const isDevelopment = process.env.NODE_ENV === 'development'
+
+  // Use the appropriate cookie name based on the environment
+  const cookieName = isDevelopment
+    ? 'next-auth.session-token'
+    : '__Secure-next-auth.session-token'
+
+  const nextAuthSession = getCookies.get(cookieName)?.value || ''
 
   return NextResponse.json(nextAuthSession)
 }
